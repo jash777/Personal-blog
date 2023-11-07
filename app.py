f4ff15037833e1d6bde7b4e1e5d2fcf5bc6435b2
jash777 <jashuva.peyyala97@gmail.com>
2023-11-07 13:15:45 +0530

    post-4

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def post2():
 def post3():
     return render_template('post3.html')
 
+@app.route('/post4')
+def post4():
+    return render_template('post4.html')
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True,host='0.0.0.0')
